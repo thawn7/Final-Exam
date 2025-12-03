4b9784ce6e7464f512e26b17dbf557d998f41ea1
thawn7 <thwvnn@gmail.com>
2025-12-02 22:07:22 -0600

    update

---
+++ Assets/scripts/ControlPlayer.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlPlayer : MonoBehaviour
+{
+    float speed, rotatioAroundY;
+    Animator anim;
+    CharacterController controller;
+    AnimatorStateInfo info;
+    bool isTalking = false;
+
+    GameObject objectToPickUp;
+    bool itemToPickUpNearBy = false;
+    GameObject userMessage;
+
+    GameObject healthUI, skillsUI, shopUI;
+    [Header("Health Settings")]
+    [Tooltip("Health value between 0 and 100")]
+    public int health = 50;
+
+    public bool shopIsDisplayed;
+    GameObject weapon;
+    bool weaponIsActive = false;
+
+    // Start is called before the first frame update
+    //public void IncreaseHealth (int amount)
+    //{
+
+    // health += amount;
+    //if (health > 100) health = 100;
+    //print("Health: " + health);
+    //GameObject.Find("healthBar").GetComponent<ManageBar>().SetValue(health);
+
+    //}
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+
+        // userMessage = GameObject.Find("userMessage");
+        //userMessage.SetActive(false);
+
+        // GameObject.Find("healthBar").GetComponent<ManageBar>().SetValue(health);
+        //  shopUI = GameObject.Find("shopUI");
+        //  shopUI.SetActive(false);
+        // weapon = GameObject.Find("playerWeapon").gameObject; weapon.SetActive(false);
+        // }
+
+        // Update is called once per frame
+    }
+    void Update()
+    {
+        if (isTalking) return;
+        info = anim.GetCurrentAnimatorStateInfo(0);
+        speed = Input.GetAxis("Vertical");
+        rotatioAroundY = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", speed);
+        gameObject.transform.Rotate(0, rotatioAroundY, 0);
+        if (speed > 0) controller.Move(transform.forward * speed * 2.0f * Time.deltaTime);
+
+
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.gameObject.name == "Diana" && !isTalking)
+        {
+
+            hit.collider.gameObject.GetComponent<DialogueSystem>().startDialogue();
+            isTalking = true;
+            anim.SetFloat("speed", 0);
+            hit.collider.isTrigger = true;
+            hit.collider.gameObject.GetComponent<BoxCollider>().size = new Vector3(2, 1, 2);
+
+        }
+    }
+    public void EndTalking()
+    {
+
+        isTalking = false;
+    }
+}
---
+++ Assets/scripts/DialogueSystem.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    string nameOfCharacter;
+    Dialogue[] dialogues;
+    int nbDialogues;
+    int currentDialogueIndex = 0;
+    bool waitingForUserInput = false;
+    bool dialogueIsActive = false;
+
+    GameObject dialogueBox, dialoguePanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameOfCharacter = gameObject.name;
+        nbDialogues = calculateNbDialogues();
+        dialogues = new Dialogue[nbDialogues];
+
+        loadDialogues();
+        nbDialogues = calculateNbDialogues();
+        //for (int i = 0; i < nbDialogues; i++)
+        //{
+
+
+          //  print("Message:" + dialogues[i].message);
+          //  print("Answer A:" + dialogues[i].response[0]);
+          //  print("Answer B:" + dialogues[i].response[1]);
+        //}
+
+        dialogueBox = GameObject.Find("dialogueBox");
+        dialoguePanel = GameObject.Find("dialoguePanel");
+        GameObject.Find("dialogueImage").GetComponent<RawImage>().texture = Resources.Load<Texture2D>(gameObject.name) as Texture2D;
+        dialoguePanel.SetActive(false);
+
+
+
+        //startDialogue();
+
+    }
+
+
+    public void loadDialogues()
+    {
+
+        TextAsset textAsset = (TextAsset)Resources.Load("dialogues");
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(textAsset.text);
+        int dialogueIndex = 0;
+
+        foreach (XmlNode character in doc.SelectNodes("dialogues/character"))
+        {
+            if (character.Attributes.GetNamedItem("name").Value == nameOfCharacter)
+            {
+
+                dialogueIndex = 0;
+                foreach (XmlNode dialogueFromXML in doc.SelectNodes("dialogues/character/dialogue"))
+                {
+                    dialogues[dialogueIndex] = new Dialogue();
+                    dialogues[dialogueIndex].message = dialogueFromXML.Attributes.GetNamedItem("content").Value;
+                    int choiceIndex = 0;
+                    dialogues[dialogueIndex].response = new string[2];
+                    dialogues[dialogueIndex].targetForResponse = new int[2];
+                    foreach (XmlNode choice in dialogueFromXML)
+                    {
+
+                        dialogues[dialogueIndex].response[choiceIndex] = choice.Attributes.GetNamedItem("content").Value;
+                        dialogues[dialogueIndex].targetForResponse[choiceIndex] = int.Parse(choice.Attributes.GetNamedItem("target").Value);
+                        choiceIndex++;
+
+                    }
+                    dialogueIndex++;
+
+                }
+
+            }
+
+
+        }
+
+    }
+
+
+    public int calculateNbDialogues()
+    {
+
+        TextAsset textAsset = (TextAsset)Resources.Load("dialogues");
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(textAsset.text);
+        int dialogueIndex = 0;
+
+        foreach (XmlNode character in doc.SelectNodes("dialogues/character"))
+        {
+            if (character.Attributes.GetNamedItem("name").Value == nameOfCharacter)
+            {
+                foreach (XmlNode dialogueFromXML in doc.SelectNodes("dialogues/character/dialogue"))
+                {
+
+                    dialogueIndex++;
+                }
+
+            }
+
+        }
+
+        return dialogueIndex;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (dialogueIsActive)
+        {
+            if (!waitingForUserInput)
+            {
+                dialoguePanel.SetActive(true);
+                if (currentDialogueIndex != -1) displayDialogue2();
+                else
+                {
+
+                    dialogueIsActive = false;
+                    dialoguePanel.SetActive(false);
+                    waitingForUserInput = false;
+                    GameObject.Find("Player").GetComponent<ControlPlayer>().EndTalking();
+
+                    currentDialogueIndex = 0;
+
+                    //GameObject.Find("GameManager").GetComponent<QuestSystem>().Notify(QuestSystem.possibleActions.talk_to, nameOfCharacter);
+
+
+                }
+                waitingForUserInput = true;
+
+
+
+
+            }
+
+            else
+            {
+
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+
+                    currentDialogueIndex = dialogues[currentDialogueIndex].targetForResponse[0];
+                    waitingForUserInput = false;
+
+                }
+                else if (Input.GetKeyDown(KeyCode.B))
+                {
+                    currentDialogueIndex = dialogues[currentDialogueIndex].targetForResponse[1];
+                    waitingForUserInput = false;
+
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+
+    public void displayDialogue2()
+    {
+
+        string textToDisplay = "[" + gameObject.name + "]" + " " + dialogues[currentDialogueIndex].message + "\n [A]> " + dialogues[currentDialogueIndex].response[0] + "\n [B]> " + dialogues[currentDialogueIndex].response[1];
+        GameObject.Find("dialogueBox").GetComponent<Text>().text = textToDisplay;
+
+
+    }
+
+    public void displayDialogue1()
+    {
+
+        print(dialogues[currentDialogueIndex].message);
+        print("[A]" + dialogues[currentDialogueIndex].response[0]);
+        print("[B]" + dialogues[currentDialogueIndex].response[1]);
+
+    }
+
+    public void startDialogue()
+    {
+        waitingForUserInput = false;
+        dialogueIsActive = true;
+
+
+    }
+}
