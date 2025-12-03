8549b292b7d10256910e70b6c8b5d2350ae23ccd
thawn7 <thwvnn@gmail.com>
2025-12-02 23:54:02 -0600

    update

---
+++ Assets/scripts/ShopItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    string name;
+    int price, quantity;
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        quantity = 0;
+        UpdateQuantityLabel();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseQuantity()
+    {
+        if (!canClick()) return;
+        quantity++;
+        UpdateQuantityLabel();
+
+
+    }
+
+
+    public void DecreaseQuantity()
+    {
+        quantity--;
+        if (quantity < 0) quantity = 0;
+        UpdateQuantityLabel();
+    }
+
+    void UpdateQuantityLabel()
+    {
+        transform.Find("itemQty").GetComponent<Text>().text = "" + quantity;
+
+        GameObject.Find("shopSystem").GetComponent<ShopSystem>().UpdateTotal(index, quantity);
+
+
+    }
+
+    bool canClick()
+    {
+
+        ///return true;
+        /// 
+        GameObject shopSystem = GameObject.Find("shopSystem");
+        return shopSystem.GetComponent<ShopSystem>().canAddItemsTocart(this.index);
+
+    }
+
+}
