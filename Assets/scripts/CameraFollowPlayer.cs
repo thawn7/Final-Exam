9870142197ad42a0aae05576906a85ee51e6e80a
thawn7 <thwvnn@gmail.com>
2025-12-04 01:39:44 -0600

    update

--- Assets/scripts/CameraFollowPlayer.cs
+++ Assets/scripts/CameraFollowPlayer.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     GameObject target;
-    //public bool indoorMode = false;
-    //GameObject closerangeCamera;
+    public bool indoorMode = false;
+    GameObject closerangeCamera;
     // Start is called before the first frame update
     void Start()
     {
         target = transform.parent.gameObject;
-    //    closerangeCamera = GameObject.Find("cameraIndoor");
-    //    closerangeCamera.SetActive(false);
+        closerangeCamera = GameObject.Find("cameraIndoor");
+        closerangeCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(target.transform.position);
-        //if (Input.GetKeyDown(KeyCode.C)) indoorMode = !indoorMode;
-       /// if (indoorMode)
-       // {
-       //     closerangeCamera.SetActive(true);
-//
-       // }
-      //  else
-      //  {
+        if (Input.GetKeyDown(KeyCode.C)) indoorMode = !indoorMode;
+        if (indoorMode)
+        {
+            closerangeCamera.SetActive(true);
 
-     //       if (closerangeCamera.active) closerangeCamera.SetActive(false);
+        }
+        else
+        {
 
-      
+            if (closerangeCamera.activeSelf) closerangeCamera.SetActive(false);
+
+        }
     }
 }
